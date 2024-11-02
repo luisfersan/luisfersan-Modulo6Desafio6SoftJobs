95b554a0b723efe39b3cf4168192496ced41cfb6
luisfersan <lfersan@gmail.com>
2024-11-01 19:14:33 -0500

    doc/Se agrega img del proyecto al README

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -5,14 +5,15 @@ import { ENDPOINT } from '../config/constans'
 import Context from '../contexts/Context'
 
 const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
-const initialForm = { email: 'docente@desafiolatam.com', password: '123456' }
+const initialForm = { email: '', password: '' }
 
 const Login = () => {
   const navigate = useNavigate()
   const [user, setUser] = useState(initialForm)
   const { setDeveloper } = useContext(Context)
 
-  const handleUser = (event) => setUser({ ...user, [event.target.name]: event.target.value })
+  const handleUser = (event) =>
+    setUser({ ...user, [event.target.name]: event.target.value })
 
   const handleForm = (event) => {
     event.preventDefault()
@@ -25,7 +26,8 @@ const Login = () => {
       return window.alert('El formato del email no es correcto!')
     }
 
-    axios.post(ENDPOINT.login, user)
+    axios
+      .post(ENDPOINT.login, user)
       .then(({ data }) => {
         window.sessionStorage.setItem('token', data.token)
         window.alert('Usuario identificado con éxito 😀.')
@@ -39,32 +41,40 @@ const Login = () => {
   }
 
   return (
-    <form onSubmit={handleForm} className='col-10 col-sm-6 col-md-3 m-auto mt-5'>
+    <form
+      onSubmit={handleForm}
+      className="col-10 col-sm-6 col-md-3 m-auto mt-5"
+    >
       <h1>Iniciar Sesión</h1>
       <hr />
-      <div className='form-group mt-1 '>
+      <div className="form-group mt-1 ">
         <label>Email address</label>
         <input
           value={user.email}
           onChange={handleUser}
-          type='email'
-          name='email'
-          className='form-control'
-          placeholder='Enter email'
+          type="email"
+          name="email"
+          className="form-control"
+          placeholder="Enter email"
         />
       </div>
-      <div className='form-group mt-1 '>
+      <div className="form-group mt-1 ">
         <label>Password</label>
         <input
           value={user.password}
           onChange={handleUser}
-          type='password'
-          name='password'
-          className='form-control'
-          placeholder='Password'
+          type="password"
+          name="password"
+          className="form-control"
+          placeholder="Password"
         />
       </div>
-      <button type='submit' className='btn btn-light mt-3'>Iniciar Sesión</button>
+      <button
+        type="submit"
+        className="btn btn-light mt-3"
+      >
+        Iniciar Sesión
+      </button>
     </form>
   )
 }
